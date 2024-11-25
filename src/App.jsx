10d0d164f68fd6c11853344df912a54b48a115ad
giovanni-lumini <giovanni.lumini28@gmail.com>
2024-11-25 11:52:13 +0100

    DefaultLayout

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Books from "./pages/Books"
 import Contacts from "./pages/Contacts"
+import DefaultLayout from "./components/DefaultLayout"
 
 import './App.css'
 
@@ -12,10 +13,13 @@ function App() {
       <BrowserRouter>
         {/* routes here */}
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/books" element={<Books />} />
-          <Route path="/contacts" element={<Contacts />} />
+          <Route element={<DefaultLayout />} >
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/books" element={<Books />} />
+            <Route path="/contacts" element={<Contacts />} />
+          </Route>
+
         </Routes>
       </BrowserRouter>
     </>
