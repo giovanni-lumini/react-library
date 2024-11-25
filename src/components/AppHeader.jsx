6d7e11bc0bed857f147181f90b0fcc470fc26af4
giovanni-lumini <giovanni.lumini28@gmail.com>
2024-11-25 12:08:21 +0100

    components

---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,12 @@
+import MainMenu from "./MainMenu"
+
+export default function AppHeader() {
+    return (
+        <header>
+            <div className="logo">
+                LOGO
+            </div>
+            <MainMenu />
+        </header>
+    )
+}
--- src/components/DefaultLayout.jsx
+++ src/components/DefaultLayout.jsx
@@ -1,21 +1,17 @@
 import { Outlet } from "react-router-dom"
-import MainMenu from "../components/MainMenu"
+import AppHeader from "../components/AppHeader"
+import AppFooter from "../components/AppFooter"
 
 export default function DefaultLayout() {
     return (
         <>
-            <header>
-                <div className="logo">
-                    LOGO
-                </div>
-                <MainMenu />
-            </header>
+            <AppHeader />
+
             <main>
                 <Outlet />
             </main>
-            <footer>
-                <p>Copiright 2024</p>
-            </footer>
+
+            <AppFooter />
         </>
     )
 }
